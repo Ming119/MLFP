b9eee8de524f8bcfb6daac0718d1739e44201568
Ming <ming2007119@gmail.com>
2023-11-25 16:57:25 +0800

    fix utils

--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import os
 import json
 import numpy as np
-import torch
 
 def _time2int(time: str) -> int:
   return int(time[:2]) * 60 + int(time[2:])
@@ -11,7 +10,7 @@ def _read_json(path: str, step_minute: int = 1) -> list:
     print(f'Reading {path}...')
     return [value['sbi'] for key, value in json.load(f).items() if value and _time2int(key) % step_minute == 0]
 
-def read_data(head_folder: str):
+def read_data(head_folder: str, step_minute: int = 1):
   total_stops = []
   first_folder = sorted(os.listdir(f'./{head_folder}'))[0]
 
@@ -24,13 +23,8 @@ def read_data(head_folder: str):
     stationData = []
     for file in sorted(os.listdir(f'./{head_folder}/{folder}')):
       if not file.endswith('.json'): continue
-      stationData.append(_read_json(f'{head_folder}/{folder}/{file}'))
+      stationData.append(_read_json(f'{head_folder}/{folder}/{file}', step_minute))
     stationData = np.transpose(np.array(stationData))
     data = np.append(data, stationData)
   data = np.reshape(data, (-1, 112))
   return data, total_stops
-
-def create_dataset(data: np.array, lookback: int):
-  dataX = np.array([data[i:(i + lookback)] for i in range(len(data) - lookback)])
-  dataY = data[lookback:]
-  return torch.tensor(dataX), torch.tensor(dataY)
